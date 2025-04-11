3e9adab8abc46da25c5115b4294bfb0b1a1df042
Dorian <tianqiyin013368@gmail.com>
2025-04-11 13:19:39 +0800

    Add createCounter function with increment, decrement, and reset methods

--- js_practice.js
+++ js_practice.js
@@ -20,3 +20,34 @@ var expect = function (val) {
     },
   };
 };
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  const original = init;
+  let _init = init;
+
+  return {
+    increment: () => {
+      _init = _init + 1;
+      return _init;
+    },
+    decrement: () => {
+      _init = _init - 1;
+      return _init;
+    },
+    reset: () => {
+      _init = original;
+      return _init;
+    },
+  };
+};
+
+const counter = createCounter(5);
+const a = counter.increment(); // 6
+const b = counter.reset(); // 5
+const c = counter.decrement(); // 4
+
+console.log(a, b, c);
