bf9cf4708699b55403332373a6961ccd9ac1e825
Dorian <tianqiyin013368@gmail.com>
2025-04-12 17:08:33 +0800

    Add timeLimit function to enforce a maximum execution time for asynchronous functions

--- js_practice.js
+++ js_practice.js
@@ -229,3 +229,20 @@ var cancellable = function (fn, args, t) {
 
   return cancelFn;
 };
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    const timeout = new Promise((_, reject) => {
+      setTimeout(() => {
+        reject('Time Limit Exceeded');
+      }, t);
+    });
+
+    return Promise.race([fn(...args), timeout]);
+  };
+};
