d239acf2b5bfa157a6a94cf3767b326e87f6e65b
Dorian <tianqiyin013368@gmail.com>
2025-04-12 20:45:16 +0800

    Add join function to merge two arrays of objects based on their IDs, combining properties when IDs match

--- js_practice.js
+++ js_practice.js
@@ -414,3 +414,26 @@ Array.prototype.groupBy = function (fn) {
 var sortBy = function (arr, fn) {
   return arr.sort((a, b) => fn(a) - fn(b));
 };
+
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+  const result = {};
+
+  arr1.forEach((item) => {
+    result[item.id] = item;
+  });
+
+  arr2.forEach((item) => {
+    if (result[item.id]) {
+      result[item.id] = { ...result[item.id], ...item };
+    } else {
+      result[item.id] = item;
+    }
+  });
+
+  return Object.values(result);
+};
