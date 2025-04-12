84fb6b1c402a3ce45239370c35cb5f5490a09ed0
Dorian <tianqiyin013368@gmail.com>
2025-04-12 20:12:24 +0800

    Add last method to Array prototype to retrieve the last element or -1 for empty arrays

--- js_practice.js
+++ js_practice.js
@@ -381,3 +381,11 @@ var chunk = function (arr, size) {
 
   return result;
 };
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+  if (this.length === 0) return -1;
+  return this[this.length - 1];
+};
