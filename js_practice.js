b3d788978de41fcd7b672fdca83393aee0ed3da1
Dorian <tianqiyin013368@gmail.com>
2025-04-12 16:02:04 +0800

    Add addTwoPromises function to resolve two promises and return their sum

--- js_practice.js
+++ js_practice.js
@@ -167,3 +167,12 @@ function memoize(fn) {
     return result;
   };
 }
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+  return Promise.all([promise1, promise2]).then(([a, b]) => a + b);
+};
