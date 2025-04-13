cf3df90ec1bc824953aa5849083f1d98025ed010
Dorian <tianqiyin013368@gmail.com>
2025-04-13 13:42:46 +0800

    Add ArrayWrapper class to encapsulate an array and provide valueOf and toString methods for custom behavior

--- js_practice.js
+++ js_practice.js
@@ -545,3 +545,39 @@ console.log('sub1: ', sub1);
 const sub2 = emitter.subscribe('firstEvent', (x) => x + 2);
 sub2.unsubscribe(); // undefined
 emitter.emit('firstEvent', [5]); // [7]
+
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function (nums) {
+  this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function () {
+  if (this.nums.length === 0) return 0;
+  let value = 0;
+  for (let i = 0; i < this.nums.length; i++) {
+    value += this.nums[i];
+  }
+
+  return value;
+};
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function () {
+  return `[${this.nums.join(',')}]`;
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
